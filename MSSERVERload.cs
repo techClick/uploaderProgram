bb965f883031afe5ed335e2c45f8381d3f755bac
techClick <36984782+techClick@users.noreply.github.com>
2019-03-30 10:16:40 +0100

    Update MSSERVERload.cs

--- MSSERVERload.cs
+++ MSSERVERload.cs
@@ -12,7 +12,7 @@ namespace uploader
         private SqlDataReader dr;
         public void saveData(string name, string email, object docs, string resultNum)
         {
-            ConnectionString = @"Server=." + "\\" + "SQLEXPRESS;Database=reodumm;uid=reallyty;pwd=esnoyy";
+            ConnectionString = @"Server=." + "\\" + "SQLTIN;Database=reodumm;uid=reallyty;pwd=esnoyy";
             cn = new SqlConnection(ConnectionString);
             try
             {
